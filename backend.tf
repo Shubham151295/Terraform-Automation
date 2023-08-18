5674a87d0652f30916b4fb62ca26591329e5c028
Shubham151295 <134634309+Shubham151295@users.noreply.github.com>
2023-08-19 01:09:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform"
+    bucket = "mydev-tf-state-bucket-project-terraform1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
